ead3564e6cd1ec8f68c7601181debbbfb107983d
Masashi SAKAI <masashi.sakai1986@gmail.com>
2022-05-20 14:36:42 +0900

    Spring Initializrで生成したファイルを追加

---
+++ src/main/java/com/github/massakai/springkafkasink/SpringKafkaSinkApplication.java
@@ -0,0 +1,13 @@
+package com.github.massakai.springkafkasink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaSinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/massakai/springkafkasink/SpringKafkaSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.massakai.springkafkasink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
