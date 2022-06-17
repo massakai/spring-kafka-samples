33150ba6e4439b4f8e7c22c08b403f9929c42de6
Masashi SAKAI <masashi.sakai1986@gmail.com>
2022-06-17 11:11:18 +0900

    雛形を追加

---
+++ producer-consumer-sample/src/test/java/com/github/springkafkasamples/producerconsumersample/ProducerConsumerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springkafkasamples.producerconsumersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerConsumerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
